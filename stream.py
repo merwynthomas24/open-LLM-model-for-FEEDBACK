a4027f1beaeab7886721e32e2a6384df5a4a30c3
merwynthomas24 <167561293+merwynthomas24@users.noreply.github.com>
2024-06-22 22:36:00 +0530

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,38 @@
+
+from transformers import pipeline
+from transformers import AutoTokenizer,AutoModelForSequenceClassification
+import torch
+import numpy as np
+tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+
+import streamlit as st
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    @st.cache(allow_output_mutation=True)
+    def get_model():
+     tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+     model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+     return tokenizer,model
+
+
+tokenizer,model = get_model()
+
+user_input = st.text_area('Enter Text to Analyze')
+button = st.button("Analyze")
+
+d = {
+    
+   1:'Postive',
+   0.5:'Netural',
+   0:'Negative'
+}
+
+if user_input and button :
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+    # test_sample
+    output = model(**test_sample)
+    st.write("Logits: ",output.logits)
+    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
+    st.write("Prediction: ",d[y_pred[0]])
